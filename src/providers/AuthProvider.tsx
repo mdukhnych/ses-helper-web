66356c062d92d5c4db0badf2d3d744dd3dd0b736
Mykola Dukhnych <mykola.dukhnych@gmail.com>
2025-07-23 16:27:12 +0300

    add spiner to auth provider

--- src/providers/AuthProvider.tsx
+++ src/providers/AuthProvider.tsx
@@ -1,16 +1,19 @@
 'use client'
 
 import { onAuthStateChanged } from "firebase/auth";
-import { ReactNode, useEffect } from "react"
+import { ReactNode, useEffect, useState } from "react"
 import { FIREBASE_AUTH } from "../../firebaseConfig";
 import { getUserData } from "@/services";
 import { IUserState } from "@/types";
 import { useDispatch } from "react-redux";
 import { useRouter } from "next/navigation";
 import { setUser } from "@/store/slices/userSlice";
+import Spinner from "@/components/ui/Spinner/Spinner";
 
 
 export default function AuthProvider({ children }: {children: ReactNode}) {  
+  const [loading, setLoading] = useState<boolean>(true)
+
   const dispatch = useDispatch()
   const router = useRouter()
   
@@ -19,13 +22,19 @@ export default function AuthProvider({ children }: {children: ReactNode}) {
       if (user) {
         const userData: IUserState = await getUserData(user.uid) as IUserState
         dispatch(setUser(userData))
+        setLoading(false)
       } else {
         router.replace('/login')
+        setLoading(false)
       }
     })
   }, [dispatch, router])
 
   return (
-    <>{children}</>
+    <>
+      {
+        loading ? <Spinner size={100} borderWidth={10} center additionalStyles={{margin: "50px 0"}}/> : children
+      }
+    </>
   )
 }
